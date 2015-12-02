0ebe03637a6742f86e323da218e6e5fb6af7a7bf
Mory Friedman <friedmanmory2@gmail.com>
2015-12-01 19:12:43 -0600

    Adding Quantity Inputs

--- scripts/bundle.js
+++ scripts/bundle.js
@@ -9217,6 +9217,9 @@ var $ = require('jquery');
 var item1 = $('#item1');
 var item2 = $('#item2');
 var item3 = $('#item3');
+var item4 = $('#item4');
+var item5 = $('#item5');
+var item6 = $('#item6');
 var subtotal = $('#subtotal');
 var submit = $('#submit');
 var button = $('#button');
@@ -9226,11 +9229,14 @@ var total = $('#total');
 function calculate(e) {
 	e.preventDefault();
 
-	if (item1.val() > 0 && item2.val() > 0 && item3.val() > 0) {
+	if (item1.val() >= 0 && item2.val() >= 0 && item3.val() >= 0 && item4.val() >= 0 && item5.val() >= 0 && item6.val() >= 0) {
 		var item1Value = parseFloat(item1.val());
 		var item2Value = parseFloat(item2.val());
 		var item3Value = parseFloat(item3.val());
-		var subtotalValue = item1Value + item2Value + item3Value;
+		var item4Value = parseFloat(item4.val());
+		var item5Value = parseFloat(item5.val());
+		var item6Value = parseFloat(item6.val());
+		var subtotalValue = item1Value * item2Value + item3Value * item4Value + item5Value * item6Value;
 		$('#subtotal').val(subtotalValue);
 	}
 }
